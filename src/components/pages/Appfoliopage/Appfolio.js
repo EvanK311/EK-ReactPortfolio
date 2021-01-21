a6425ca89632f947fbddf46af314895489653b03
Evan K <EvanK311@gmail.com>
2021-01-21 12:14:28 -0800

    updated picture  and Appfolio appearance

--- src/components/pages/Appfoliopage/Appfolio.js
+++ src/components/pages/Appfoliopage/Appfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Appfolio.css"
 import password from "../../../pics/Capture.PNG"
-import forcaster from "../../../pics/resultscreen.PNG"
+import wislogo from "../../../pics/wisyrdColor.png"
 import discovery from "../../../pics/venu-logo-large.png"
 import petwork from "../../../pics/petwork.png"
 import scheduler from "../../../pics/scheduler.PNG"
@@ -13,13 +13,13 @@ function Appfolio() {
     <div className="bigcontainer">
         <row className="row">
             <column className="col-md-12">
-            <h1>Portfolio</h1>
+            <h1 className="center">Portfolio</h1>
             </column>
         </row>
         <row className="row">
-            <column className="col-md-6- col-lg-6 col-xs-6">
+            <column className="col-md-6 col-lg-6 col-xs-6">
                 <h5><a href = "https://ajm5099.github.io/Music-Discovery/">VenU - Music Discovery</a></h5>
-                <img src={discovery} id="pic1" className="PortPic img-fluid img-thumbnail" alt="venU logo"/></column>
+                <img src={discovery} id="pic1" className="marginbuff PortPic img-fluid img-thumbnail" alt="venU logo"/></column>
             <column className="column-md-6 col-lg-6 col-xs-6">
                 <h5><a href ="https://secret-meadow-51163.herokuapp.com/">The Social Petwork</a></h5>
                 <img src={petwork} id="pic2" className="PortPic img-fluid img-thumbnail" alt="screen of social petwork app"/>
@@ -28,11 +28,12 @@ function Appfolio() {
       <row className="row">
             <column className="column-md-6 col-lg-6 col-xs-6">
                 <h5><a href = "https://evank311.github.io/EK-Password-Gen/">Password Generator</a></h5>
-                <img src={password} id="pic3" className="PortPic img-fluid img-thumbnail" alt="password generator screen"/>
+                <img src={password} id="pic3" className="marginbuff PortPic img-fluid img-thumbnail" alt="password generator screen"/>
             </column>
             <column className="column-md-6 col-lg-6 col-xs-6">
-               <h5><a href = "https://evank311.github.io/EK-forcaster/" >Simple Weather Forcaster</a></h5>
-                <img src={forcaster} id="pic4" className="PortPic img-fluid img-thumbnail" alt="screen of weather app"/>
+                  {/* placeholder for Wisyrd */}
+                  <h5><a href = "https://wisyrd.herokuapp.com/">Wisyrd</a></h5>
+                <img src={wislogo} id="pic3" className="PortPic img-fluid img-thumbnail" alt="Wisyrd logo"/>
             </column>
         </row>       
         <row className="row">
